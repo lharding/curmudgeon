0879b7d5e311c17cb452cb176739b5459909a53c
Leander Harding <leander@fremde.org>
2012-02-12 12:34:34 -0800

    Ready for github now

--- curmudgeon_conf.py
+++ curmudgeon_conf.py
@@ -1,7 +1,7 @@
 POSTS_DIR = 'blog'
 SUFFIX = 'blog'
 TEMPLATE_DIR = 'templates'
-OUTPUT_DIR = '../out'
+OUTPUT_DIR = 'out'
 SKELETON_DIR = 'skel'
 
 FRONTPAGE_FILE = 'index.html'
@@ -9,7 +9,7 @@ ARCHIVE_FILE = 'archive.html'
 FEED_FILE = 'feed.xml'
 
 POST_SUFFIX = '.html'
-URL_PREFIX = ''
+URL_PREFIX = '/' #what you want for hosting from a web server. Make it blank to see the frontpage from a local file.
 
 BLOG_TITLE = 'Curmudgeonly Blog'
 BLOG_HOST = 'my.host.invalid'
